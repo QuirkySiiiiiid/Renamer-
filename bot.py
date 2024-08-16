9e520d78d68f7dddd3fef7c38112f802ed62e7fe
QuirkySiiiiiid <jainjainja112233@gmail.com>
2024-08-16 12:40:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import asyncio
 import datetime
 import subprocess
 import time
-from pyrofork import Client
+from pyrogram import Client
+from pyrogram.errors import BadMsgNotification
 from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB, PORT
 from aiohttp import web
 from plugins.web_support import web_server
